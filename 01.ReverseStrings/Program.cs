552277f5718ab4f4b9e80d17d49f5556b8ec2682
Ivan Barzinski <Barzinski@students.softuni.bg>
2025-03-06 21:51:21 +0200

    Add project files.

---
+++ 01.ReverseStrings/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+
+namespace _01.ReverseStrings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+
+            StringBuilder sb = new StringBuilder();
+
+            while (text != "end")
+            {
+                sb.Clear();
+
+                for (int i = text.Length -1; i >= 0; i--)
+                {
+                    sb.Append(text[i]);
+                }
+
+                Console.WriteLine($"{text} = {sb}");
+
+                text = Console.ReadLine();
+            }
+            
+        }
+    }
+}
---
+++ 03.Substring/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace _03.Substring
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+            string text = Console.ReadLine();
+
+            int index = text.IndexOf(word);
+
+            while (index != -1)
+            {
+                text = text.Remove(index, word.Length);
+                index = text.IndexOf(word);
+            }
+
+            Console.WriteLine(text);
+        }
+    }
+}
